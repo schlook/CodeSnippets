6b0408a4d3ac15ff5daef2810daee3d24650177d
schlook <knasen1@gmail.com>
2022-04-25 11:05:16 +0200

    first commit

---
+++ RockScissorPaper/inc/include.hpp
@@ -0,0 +1,18 @@
+#ifndef INCLUDE_HPP_
+#define INCLUDE_HPP_
+// We only want this file to be included once
+#pragma once
+// Standard input and output streams
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <limits>
+#include <random>
+// std::this_thread::sleep_for
+#include <thread>
+// std::chrono::seconds
+#include <chrono>
+
+#include "game.hpp"
+
+#endif
---
+++ RockScissorPaper/src/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: Niklas 'schlook' Karlsson
+ * Description:
+ * Rock, scissor and paper game.
+ */
+
+// I collect all the include files
+// That is needed for the project here.
+#include "include.hpp"
+
+// Declare returntype 0
+auto main(int argc, const char **argv) -> decltype(0)
+// Try someting
+try
+{
+    game *Game = new game();
+
+    // Endless loop
+    while (true)
+    {
+        Game->start();
+        Game->showResult();
+    }
+    // Same as 0
+    return 0x0;
+}
+// If we fail and threw something.
+catch (const char *e)
+{
+    std::cerr << "[ERROR] " << e << std::endl;
+}
+catch (...)
+{
+    std::cerr << "[ERROR] unexpected error!" << std::endl;
+}
